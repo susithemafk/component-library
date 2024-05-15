80765f17f20f75cc3e3742d271c138122c875d2c
marek.sucharda <marek.sucharda@asseco-ce.com>
2024-05-15 11:17:49 +0200

    dialog storybook

--- lib/Dialog/Dialog.stories.tsx
+++ lib/Dialog/Dialog.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Dialog } from './Dialog'
+import { Dialog, DialogProps } from './Dialog'
 import '../../src/styles/main.scss'
+import { useState } from 'react'
 
 const meta: Meta<typeof Dialog> = {
     title: 'Dialog',
@@ -9,3 +10,52 @@ const meta: Meta<typeof Dialog> = {
 export default meta
 
 type Story = StoryObj<typeof Dialog>
+type StoryFunctional = StoryObj<DialogProps>
+
+export const Default: Story = {
+    args: {
+        children: <strong>Default dialog</strong>,
+        open: true,
+    },
+}
+
+export const OverlayNone: Story = {
+    args: {
+        children: <div>Overlay none dialog</div>,
+        open: true,
+        overlay: 'none',
+    },
+}
+
+export const OverlayMedium: Story = {
+    args: {
+        children: 'Overlay medium dialog',
+        open: true,
+        overlay: 'medium',
+    },
+}
+
+export const OverlayDark: Story = {
+    args: {
+        children: <strong>Overlay dark dialog</strong>,
+        open: true,
+        overlay: 'dark',
+    },
+}
+
+export const ControlledDialog: StoryFunctional = (args: DialogProps) => {
+    const [open, setOpen] = useState(false)
+    const handleClose = () => setOpen(false)
+
+    return (
+        <div>
+            <button onClick={() => setOpen(true)}>Open Dialog</button>
+            <Dialog {...args} open={open} onClose={handleClose} />
+        </div>
+    )
+}
+
+ControlledDialog.args = {
+    children: <strong>On close dialog</strong>,
+    overlay: 'dark',
+}
